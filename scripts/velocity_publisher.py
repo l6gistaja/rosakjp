3101b5e60921389f9dc4247cf0266f638e03b389
Juks <juks@alkohol.ee>
2022-10-23 01:02:08 +0300

    multiroute support

--- scripts/velocity_publisher.py
+++ scripts/velocity_publisher.py
@@ -13,41 +13,48 @@ def main():
         
         data = json.load(data_file)
         print(data['description'])
-        rospy.sleep(data['wait_at_first'])
-        loop_rate = rospy.Rate(data['rate'])
-        counter = 1
-        countdown = 1
-        if 'cycles' in data:
-            if data['cycles'] < 1:
-                countdown = -1 # eternal loop
-            else:
-               countdown = data['cycles']
-                
-        while 1:
-            print('CYCLE ',counter)
-            counter += 1
-            for leg in data['legs']:
-                
-                starting_time = rospy.get_time()
-                if 'd' in leg:
-                    print(leg['d'])
+        
+        for route in data['routes']:
+            
+            print('===== ',route['description'],' =====')
+            rospy.sleep(route['wait_at_first'])
+            loop_rate = rospy.Rate(route['rate'])
+            counter = 1
+            countdown = 1
+            if 'cycles' in route:
+                if route['cycles'] < 1:
+                    countdown = -1 # eternal loop
                 else:
-                    print(leg['x'], ';', leg['y'], ';', leg['Z'], ';', leg['t'] ) 
-                
-                while (not rospy.is_shutdown()) and (rospy.get_time() - starting_time < leg['t']):
-                    robot_vel = Twist()
-                    robot_vel.linear.x = leg['x']
-                    robot_vel.linear.y = leg['y']
-                    robot_vel.linear.z = 0.0
-                    robot_vel.angular.x = 0.0
-                    robot_vel.angular.y = 0.0
-                    robot_vel.angular.z = leg['Z']
-                    velocity_pub.publish(robot_vel)
-                    loop_rate.sleep()
+                    countdown = route['cycles']
                     
-            if countdown > 0:
-                countdown -= 1
-            if countdown == 0 or rospy.is_shutdown():
+            while 1:
+                print('CYCLE ',counter)
+                counter += 1
+                for leg in route['legs']:
+                    
+                    starting_time = rospy.get_time()
+                    if 'd' in leg:
+                        print(leg['d'])
+                    else:
+                        print(leg['x'], ';', leg['y'], ';', leg['Z'], ';', leg['t'] ) 
+                    
+                    while (not rospy.is_shutdown()) and (rospy.get_time() - starting_time < leg['t']):
+                        robot_vel = Twist()
+                        robot_vel.linear.x = leg['x']
+                        robot_vel.linear.y = leg['y']
+                        robot_vel.linear.z = 0.0
+                        robot_vel.angular.x = 0.0
+                        robot_vel.angular.y = 0.0
+                        robot_vel.angular.z = leg['Z']
+                        velocity_pub.publish(robot_vel)
+                        loop_rate.sleep()
+                        
+                if countdown > 0:
+                    countdown -= 1
+                if countdown == 0 or rospy.is_shutdown():
+                    break
+                
+            if rospy.is_shutdown():
                 break
 
 if __name__ == '__main__':
